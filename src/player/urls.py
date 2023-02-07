820cbf60bb82b3680aaf4b5aff97f295e7305394
Maryam Roshani <maryamroshani777@gmail.com>
2023-02-07 12:45:08 +0330

    some changes

--- src/player/urls.py
+++ src/player/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import patterns, url
 from media_player import views
+from django.urls import path, include
 
 
-urlpatterns = patterns('',
-#    url(r"^(?P<media_src>[\w.,/_\-\'\!\[\]\&\% ]+)$", views.media_player, name='media_player'),
-    url(r'^(?P<media_src>.*)$',views.media_player, name='media_player'),
-)
+urlpatterns = [
+   path('admin/',views.media_player, name='media_player'),
+]
